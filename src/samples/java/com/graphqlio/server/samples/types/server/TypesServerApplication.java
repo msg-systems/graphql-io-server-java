ce7cfb72a7272c9876f1917a4f847e77c2fa05d3
kuehnert <kuehnert@msgn12426.int.root.msg.ag>
2020-01-27 13:30:33 +0100

    new scalar types sample: query and mutation

---
+++ src/samples/java/com/graphqlio/server/samples/types/server/TypesServerApplication.java
@@ -0,0 +1,89 @@
+/*******************************************************************************
+ * *
+ * **  Design and Development by msg Applied Technology Research
+ * **  Copyright (c) 2019-2020 msg systems ag (http://www.msg-systems.com/)
+ * **  All Rights Reserved.
+ * ** 
+ * **  Permission is hereby granted, free of charge, to any person obtaining
+ * **  a copy of this software and associated documentation files (the
+ * **  "Software"), to deal in the Software without restriction, including
+ * **  without limitation the rights to use, copy, modify, merge, publish,
+ * **  distribute, sublicense, and/or sell copies of the Software, and to
+ * **  permit persons to whom the Software is furnished to do so, subject to
+ * **  the following conditions:
+ * **
+ * **  The above copyright notice and this permission notice shall be included
+ * **  in all copies or substantial portions of the Software.
+ * **
+ * **  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * **  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * **  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * **  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * **  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * **  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * **  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * *
+ ******************************************************************************/
+package com.graphqlio.server.samples.types.server;
+
+import java.util.Properties;
+
+import javax.annotation.PreDestroy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.graphqlio.gts.EnableGraphQLIOGtsLibraryModule;
+import com.graphqlio.gtt.EnableGraphQLIOGttLibraryModule;
+import com.graphqlio.server.EnableGraphQLIOGsLibraryModule;
+import com.graphqlio.server.server.GsServer;
+import com.graphqlio.wsf.EnableGraphQLIOWsfLibraryModule;
+
+/**
+ * Server application for the types sample. This spring boot application set the
+ * properties and starts it. The graphql-io-server is started by the
+ * ApplicationRunner implementation. On end of spring boot application the
+ * server should be stopped.
+ * 
+ * @author Michael Schäfer
+ * @author Torsten Kühnert
+ */
+
+@SpringBootApplication
+@EnableGraphQLIOWsfLibraryModule
+@EnableGraphQLIOGttLibraryModule
+@EnableGraphQLIOGtsLibraryModule
+@EnableGraphQLIOGsLibraryModule
+public class TypesServerApplication implements ApplicationRunner {
+
+	public static void main(String[] args) {
+		Properties properties = new Properties();
+		properties.put("server.port", "8080");
+		properties.put("graphqlio.server.schemaLocationPattern", "**/*.types.graphql");
+		properties.put("graphqlio.server.endpoint", "/api/data/graph");
+		properties.put("graphqlio.toolssubscribe.useEmbeddedRedis", "true");
+		properties.put("spring.redis.host", "localhost");
+		properties.put("spring.redis.port", "26379");
+
+		SpringApplication application = new SpringApplication(TypesServerApplication.class);
+		application.setDefaultProperties(properties);
+		application.run(args);
+	}
+
+	@Autowired
+	private GsServer graphqlioServer;
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		this.graphqlioServer.start();
+	}
+
+	@PreDestroy
+	public void destroy() throws Exception {
+		this.graphqlioServer.stop();
+	}
+
+}
