49dcb999c2f043b61504c24f87ddd7b496522ca0
kuehnert <kuehnert@msgn12426.int.root.msg.ag>
2020-02-12 09:24:45 +0100

    new function deregister resolver

--- src/main/java/com/graphqlio/server/server/GsServer.java
+++ src/main/java/com/graphqlio/server/server/GsServer.java
@@ -56,4 +56,8 @@ public class GsServer {
   public void registerGraphQLResolver(GraphQLResolver resolver) {
     GtsResolverRegistry.registerGraphQLResolver(resolver);
   }
+
+  public void deregisterGraphQLResolver(GraphQLResolver resolver) {
+    GtsResolverRegistry.deregisterGraphQLResolver(resolver);
+  }
 }
